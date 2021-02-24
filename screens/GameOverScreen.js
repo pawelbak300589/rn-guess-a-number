cba76a23f70031dd3585ba76e60f46dbc436c0d6
Pawel Bak <pawelbak300589@gmail.com>
2021-02-24 19:07:42 +0000

    Update screens to line up with course files

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
@@ -7,33 +7,51 @@ import MainButton from '../components/MainButton';
 import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(Dimensions.get('window').width);
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(Dimensions.get('window').height);
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get('window').width);
+      setAvailableDeviceHeight(Dimensions.get('window').height);
+    };
+
+    Dimensions.addEventListener('change', updateLayout);
+
+    return () => {
+      Dimensions.removeEventListener('change', updateLayout);
+    };
+  });
+
   return (
     <ScrollView>
       <View style={styles.screen}>
         <TitleText>The Game is Over!</TitleText>
-        <View style={styles.imageContainer}>
+        <View style={{
+          ...styles.imageContainer, ...{
+            width: availableDeviceWidth * 0.7,
+            height: availableDeviceWidth * 0.7,
+            borderRadius: (availableDeviceWidth * 0.7) / 2,
+            marginVertical: availableDeviceHeight / 30
+          }
+        }}>
           <Image
             source={require('../assets/success.png')}
-            // source={{
-            //   uri:
-            //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
-            // }}
             style={styles.image}
             resizeMode="cover"
           />
         </View>
-        <View style={styles.resultContainer}>
-          <BodyText style={styles.resultText}>
+        <View style={{ ...styles.resultContainer, ...{ marginVertical: availableDeviceHeight / 60 } }}>
+          <BodyText style={{ ...styles.resultText, ...{ fontSize: availableDeviceHeight < 400 ? 16 : 20 } }}>
             Your phone needed{' '}
             <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-      guess the number{' '}
+                          guess the number{' '}
             <Text style={styles.highlight}>{props.userNumber}</Text>.
-      </BodyText>
+                  </BodyText>
         </View>
-
         <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-    </ScrollView>
+    </ScrollView >
   );
 };
 
@@ -41,7 +59,8 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingVertical: 10
   },
   imageContainer: {
     width: Dimensions.get('window').width * 0.7,
